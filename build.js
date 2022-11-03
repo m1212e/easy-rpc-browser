941c26e4a221cdbfe2b7a9aaedd4166b5e4721d9
m1212e <->
2022-11-03 13:19:25 +0100

    CI

--- build.js
+++ build.js
@@ -1,9 +1,12 @@
+console.log("starting build script");
 const fs = require("fs");
 
 if (!fs.existsSync("./build")) {
+    console.log("creating ./build");
     fs.mkdirSync("./build");
 }
 
+console.log("running esbuild...");
 require('esbuild').build({
     entryPoints: ['./src/main.ts'],
     bundle: true,
@@ -14,6 +17,7 @@ require('esbuild').build({
     minify: true,
     target: "node14"
 }).catch(() => process.exit(1));
+console.log("esbuild complete");
 
 // files/dirs on root level which should be included inside the build
 const include = [
@@ -21,14 +25,20 @@ const include = [
     "package-lock.json",
 ];
 
+console.log("copying files to ./build");
 function copyToBuild(name) {
+    console.log("copying " + name);
     fs.cpSync(`./${name}`, `./build/${name}`, {recursive: true});
 }
 include.forEach(name => copyToBuild(name));
+console.log("copying complete");
 
+console.log("modifying package.json");
 const pkgjson = JSON.parse(fs.readFileSync("./build/package.json"));
 if (process.env.REF_NAME) {
+    console.log("setting version to " + process.env.REF_NAME);
     pkgjson.version = process.env.REF_NAME
 }
-pkgjson.main = "main.js"
 fs.writeFileSync("./build/package.json", JSON.stringify(pkgjson))
+console.log("modifying complete");
+console.log("build script done");
